a5067bb78fb7cf5453902b092c698131eb809d48
Isaac David <orpheezt@outlook.com>
2026-04-26 21:05:00 -0500

    Add godbolt fut Dart

--- slides/src/slides/Slide01Title.tsx
+++ slides/src/slides/Slide01Title.tsx
@@ -3,7 +3,12 @@ import { Slide } from '@revealjs/react'
 export function Slide01Title() {
   return (
     <Slide>
-      <h1>Thread task placing</h1>
+      <section>
+        <h1>Thread task placing</h1>
+      </section>
+      <section>
+        <a href="https://godbolt.org/z/ThvqezxfY">Dart Async aclaration</a>
+      </section>
     </Slide>
   )
 }
